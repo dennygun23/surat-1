36a209b63c5de459e13431811955578054b0f06a
Wirasto S. Karim <wirastokarim@gmail.com>
2016-12-07 09:30:54 +0800

    Buat seed untuk user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +15,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+		$this->call(UsersTableSeeder::class);
+
+		Model::reguard();
+    }
+}
+
+
+
+class UsersTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('users')->delete();
+
+        User::create(array('name'=>'Wirasto Karim', 'email'=>'wirasto@sims.com', 'password'=> Hash::make('wirasto')));
+        User::create(array('name'=>'Lisa Kawaii', 'email'=>'lisa@sims.com', 'password'=> Hash::make('lisa02')));
     }
 }
